963d9feb946a513b819420ca825314a49feb51d3
p0nch0.eth <37346522+p0nch00@users.noreply.github.com>
2025-04-03 16:20:30 +0200

    Update et.py

--- goodwe/et.py
+++ goodwe/et.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 
 class ET(Inverter):
     """Class representing inverter of ET/EH/BT/BH or GE's GEH families AKA platform 205 or 745"""
+    comm_adress: int | None = None
 
     # Modbus registers from offset 0x891c (35100), count 0x7d (125)
     __all_sensors: Tuple[Sensor, ...] = (
@@ -470,6 +471,7 @@ class ET(Inverter):
 
     def __init__(self, host: str, port: int, comm_addr: int = 0, timeout: int = 1, retries: int = 3):
         super().__init__(host, port, comm_addr if comm_addr else 0xf7, timeout, retries)
+        self.comm_adress: int = comm_addr
         self._READ_DEVICE_VERSION_INFO: ProtocolCommand = self._read_command(0x88b8, 0x0021)
         self._READ_RUNNING_DATA: ProtocolCommand = self._read_command(0x891c, 0x007d)
         self._READ_METER_DATA: ProtocolCommand = self._read_command(0x8ca0, 0x2d)
@@ -514,7 +516,7 @@ class ET(Inverter):
         self.modbus_version = read_unsigned_int(response, 0)
         self.rated_power = read_unsigned_int(response, 2)
         self.ac_output_type = read_unsigned_int(response, 4)  # 0: 1-phase, 1: 3-phase (4 wire), 2: 3-phase (3 wire)
-        self.serial_number = self._decode(response[6:22])  # 35003 - 350010
+        self.serial_number = self._decode(response[6:22])+self.comm_adress# 35003 - 350010
         self.model_name = self._decode(response[22:32])  # 35011 - 35015
         self.dsp1_version = read_unsigned_int(response, 32)  # 35016
         self.dsp2_version = read_unsigned_int(response, 34)  # 35017
